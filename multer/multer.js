e726f70b341e5662a3799da48f8dc5a49522cad0
RopeJuve <rope.s@hotmail.com>
2024-07-18 20:11:07 +0200

    Initial Commit

--- multer/multer.js
+++ multer/multer.js
@@ -2,6 +2,8 @@ import multer from "multer";
 import path from "path";
 
 
+const __dirname = path.resolve();
+
 const storage = multer.diskStorage({
     destination: function (req, file, cb) {
       cb(null, path.join(__dirname, 'uploads'));
